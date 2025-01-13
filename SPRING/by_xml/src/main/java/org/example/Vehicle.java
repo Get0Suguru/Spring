31d9944a9caf227b84aeba09497c0e69b3f1416d
Gautam Taneja <gautam45taneja@gmail.com>
2025-01-13 11:01:19 +0530

    Introduce Vehicle interface for the XML config example

---
+++ SPRING/by_xml/src/main/java/org/example/Vehicle.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public interface Vehicle {
+    public String milage();
+
+    public String showEngine();
+
+    public String getModelName();
+    public double getCost();
+}
